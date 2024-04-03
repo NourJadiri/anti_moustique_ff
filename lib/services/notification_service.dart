64871b16b7e698299ea20dc352a33ac7a8ee90db
Isaline <isaline.foissey@gmail.com>
2024-04-03 23:01:26 +0200

    mise à jour avec flutter local notification 17.0.0

--- lib/services/notification_service.dart
+++ lib/services/notification_service.dart
@@ -1,12 +1,10 @@
-
-
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:url_launcher/url_launcher.dart';
 import '../backend/schema/structs/notification_struct.dart';
 
-
 class NotificationService {
-  late FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin;
+  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+  FlutterLocalNotificationsPlugin();
 
   static final NotificationService _instance = NotificationService._internal();
 
@@ -15,66 +13,55 @@ class NotificationService {
   NotificationService._internal();
 
   Future<void> init() async {
-    flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
-    // Configuration pour Android
-    var initializationSettingsAndroid = AndroidInitializationSettings('@mipmap/ic_launcher');
-
-    // Configuration pour iOS
-    var initializationSettingsIOS = IOSInitializationSettings(
-      requestAlertPermission: true,
-      requestBadgePermission: true,
-      requestSoundPermission: true,
-      onDidReceiveLocalNotification: (int id, String? title, String? body, String? payload) async {
-        // Ici, vous pouvez personnaliser ce que vous voulez faire lorsque la notification est reçue en premier plan
-      },
-    );
-
-    var initializationSettings = InitializationSettings(
-      android: initializationSettingsAndroid,
-      iOS: initializationSettingsIOS,
-    );
-
-    await flutterLocalNotificationsPlugin.initialize(
-      initializationSettings,
-      onSelectNotification: onSelectNotification,
-    );
+    const AndroidInitializationSettings initializationSettingsAndroid =
+    AndroidInitializationSettings('@mipmap/ic_launcher');
+    final DarwinInitializationSettings initializationSettingsDarwin =
+    DarwinInitializationSettings(
+        requestSoundPermission: false,
+        requestBadgePermission: false,
+        requestAlertPermission: false,
+        onDidReceiveLocalNotification: (int id, String? title, String? body,
+            String? payload) async {
+          // Handle foreground notification reception in iOS
+        });
+    final InitializationSettings initializationSettings = InitializationSettings(
+        android: initializationSettingsAndroid,iOS: initializationSettingsDarwin);
+    await flutterLocalNotificationsPlugin.initialize(initializationSettings,
+        onDidReceiveNotificationResponse: onDidReceiveNotificationResponse);
   }
 
   Future<void> sendLocalNotification(NotificationStruct value) async {
-    const AndroidNotificationDetails androidPlatformChannelSpecifics = AndroidNotificationDetails(
-      'votre_channel_id', // Assurez-vous que ce channel ID a été créé
-      'votre_channel_nom',
-      'votre_channel_description',
-      importance: Importance.max,
-      priority: Priority.high,
-      showWhen: false,
-    );
-    const NotificationDetails platformChannelSpecifics = NotificationDetails(android: androidPlatformChannelSpecifics);
-
+    const AndroidNotificationDetails androidPlatformChannelSpecifics =
+    AndroidNotificationDetails(
+        'your_channel_id', 'your_channel_name',
+        importance: Importance.max, priority: Priority.high, showWhen: false);
+    const NotificationDetails platformChannelSpecifics =
+    NotificationDetails(android: androidPlatformChannelSpecifics);
     await flutterLocalNotificationsPlugin.show(
-      0, // ID de la notification
-      value.antimoustique.name, // titre
-      value.body, // corps de la notification
-      platformChannelSpecifics,
-      payload: value.antimoustique.vendor,
-    );
+        0, // Dynamic ID for the notification
+        value.antimoustique.name,
+        value.body,
+        platformChannelSpecifics,
+        payload: value.antimoustique.vendor);
   }
 
-  Future onSelectNotification(String? payload) async {
+  void onDidReceiveNotificationResponse(NotificationResponse response) async {
+    String? payload = response.payload;
     print(payload);
-    if (payload == "Distributeur" || payload == "unknown") {
-      // Naviguez vers la page Map
-      final url = Uri.parse('https://www.google.com/maps/d/viewer?mid=1G4QNa9Ob5ErrTV54evRI3o_Gbm0&hl=en_US&ll=46.14750807696509%2C-71.90924343517304&z=8');
-      if (!await launchUrl(url)) {
-        throw 'Could not launch $url';
-      }
-    } else if (payload == "Pole-Habitat") {
-      // Naviguez vers le e-shop de Pole Habitat
-      // Utilisation de launchUrl pour ouvrir le navigateur
-      final url = Uri.parse('https://www.pole-habitat-ra.com');
-      if (!await launchUrl(url)) {
-        throw 'Could not launch $url';
+    if (payload != null) {
+      if (payload == "Distributeur" || payload == "unknown") {
+        final url = Uri.parse(
+            'https://www.google.com/maps/d/viewer?mid=1G4QNa9Ob5ErrTV54evRI3o_Gbm0&hl=en_US&ll=46.14750807696509%2C-71.90924343517304&z=8');
+        if (!await launchUrl(url, mode: LaunchMode.externalApplication)) {
+          throw 'Could not launch $url';
+        }
+      } else if (payload == "Pole-Habitat") {
+        final url = Uri.parse('https://www.pole-habitat-ra.com');
+        if (!await launchUrl(url, mode: LaunchMode.externalApplication)) {
+          throw 'Could not launch $url';
+        }
       }
     }
   }
 }
+
